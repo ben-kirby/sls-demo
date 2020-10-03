6c6d0d024709771a9f6041fe28101fe81b067efc
Ben Kirby <benkrby@gmail.com>
2020-10-03 11:47:28 -0700

    comments

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,5 +1,7 @@
+// Axios makes http calls easier
 import axios from 'axios'
 
+// This is the base URL that will be generated from when you deploy your serverless backend.
 const baseUrl = 'https://8m7mubiqqj.execute-api.us-east-1.amazonaws.com/dev';
 
 export const apiCall = async () => {
